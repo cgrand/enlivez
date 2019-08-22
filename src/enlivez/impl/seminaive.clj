6c5a48598a575efc00bbee659e77e3f933a9f89a
Christophe Grand <christophe@cgrand.net>
2019-08-22 13:25:24 +0200

    end-to-end seminaive evaluation of datalog with negation

--- src/enlivez/impl/seminaive.clj
+++ src/enlivez/impl/seminaive.clj
@@ -165,30 +165,30 @@
             (throw (ex-info "Unstratifiable" {:a a :b b})))
         scc-graph (scc-graph sccs (keys graph) graph)
         sorted-sccs (topo-sort sccs scc-graph)
-        rules-by-rel (group-by ffirst rules)]
-    (keep #(seq (mapcat rules-by-rel %)) sorted-sccs)))
+        rules-by-rel (group-by ffirst rules)
+        scc-rules (fn [scc]
+                    (when-some [rules (seq (mapcat rules-by-rel scc))]
+                      (let [recursive? (fn [clauses]
+                                         (some (fn [[op x]]
+                                                 (case op
+                                                   not (recur x)
+                                                   (scc op))) clauses))]
+                        (group-by (fn [[head & clauses]] (if (recursive? clauses) :recur :init)) rules))))]
+    (keep scc-rules sorted-sccs)))
 
-
-(defn seminaive-step
-  [rules scc]
-  (let [; keep only relevant rules
-        rules (filter (fn [[[op]]] (scc op)) rules)
-        recursive? (fn [clauses]
-                     (some (fn [[op x]]
-                             (case op
-                               not (recur x)
-                               (scc op))) clauses))
-        seed-rules (remove (fn [[head & clauses]] (recursive? clauses))  rules)
-        rec-rules (filter (fn [[head & clauses]] (recursive? clauses))  rules)
+(defn seminaive-stratum
+  [{recur-rules :recur init-rules :init}]
+  (let [rec-preds (into #{} (map ffirst) recur-rules)
+        ; keep only relevant rules
         inc-clauses
         (fn inc-clauses [clauses]
           (when-some [[[op & args :as clause] & more-clauses] (seq clauses)]
-            (if (scc op)
+            (if (rec-preds op)
               (let [delta-now (cons (cons [:delta op] args)
                                 (map (fn self [[op & args :as clause]]
                                        (case op
                                          not (list 'not (self (first args)))
-                                         (if (scc op)
+                                         (if (rec-preds op)
                                            (cons [:prev op] args)
                                            clause))) more-clauses))
                     delta-later (inc-clauses more-clauses)]
@@ -196,11 +196,11 @@
                   [(list 'or (cons 'and delta-now) (list* 'and clause delta-later))]
                   delta-now))
               (some->> (inc-clauses more-clauses) (cons clause)))))
-        delta-rules (for [[head & clauses] rec-rules]
+        delta-rules (for [[head & clauses] recur-rules]
                       (cons head (inc-clauses clauses)))
         ; remove ors we just added
         delta-rules (lift-ors delta-rules)]
-    {:seed seed-rules
+    {:init init-rules
      :delta delta-rules}))
 
 (defn- match [bindings pat tuple]
@@ -228,68 +228,50 @@
                 (mapcat (fn [bindings] (keep #(match bindings args %) rel)) bindings-seq))))]
     (map #(map % head-args) (reduce eval-clause [{}] clauses))))
 
-(defn eval-naive-step [db {:keys [seed delta]}]
-  (let [rec-rels (into #{} (map ffirst) delta)
-        db (into db
-             (map (fn [op]
-                    {op #{}
-                     [:prev op] #{}
-                     [:delta op] #{}}))
-             rec-rels)
-        db (transduce
-             (map (fn [[[op] :as rule]]
-                    (let [rel (set (eval-rule db rule))]
-                      {op rel
-                       [:delta op] rel})))
-             (partial merge-with into) db seed)]
-    (loop [db db]
-      (let [delta-db (transduce
-                       (map (fn [[[op] :as rule]]
-                              {op (set (eval-rule db rule))}))
-                       (partial merge-with into) {} delta)
-            db (reduce
-                 (fn [db op]
-                   (let [rel (db op)
-                         drel (remove rel (delta-db op))]
-                     (assoc db
-                       [:prev op] rel
-                       op (into rel drel)
-                       [:delta op] drel)))
-                 db rec-rels)]
-        (if (seq (mapcat #(db [:delta %]) rec-rels))
-          (recur db)
-          (reduce
-            (fn [db op] (dissoc db [:prev db] [:delta db]))
-            db rec-rels))))))
+(defn eval-seminaive-stratum [db {init-rules :init delta-rules :delta}]
+  (if-not (seq delta-rules)
+    (transduce
+      (map (fn [[[op] :as rule]]
+             {op (set (eval-rule db rule))}))
+      (partial merge-with into) db init-rules)
+    (let [rec-preds (into #{} (map ffirst) delta-rules)
+          db (into db
+               (map (fn [op]
+                      {op #{}
+                       [:prev op] #{}
+                       [:delta op] #{}}))
+               rec-preds)
+          db (transduce
+               (map (fn [[[op] :as rule]]
+                      (let [rel (set (eval-rule db rule))]
+                        {op rel
+                         [:delta op] rel})))
+               (partial merge-with into) db init-rules)]
+      (loop [db db]
+        (let [delta-db (transduce
+                         (map (fn [[[pred] :as rule]]
+                                {pred (into #{} (remove (db pred)) (eval-rule db rule))}))
+                         (partial merge-with into) {} delta-rules)
+              db (reduce
+                   (fn [db pred]
+                     (let [rel (db pred)
+                           drel (delta-db pred)]
+                       (assoc db
+                         [:prev pred] rel
+                         pred (into rel drel)
+                         [:delta pred] drel)))
+                   db rec-preds)]
+          (if (seq (mapcat delta-db rec-preds))
+            (recur db)
+            (reduce
+              (fn [db pred] (dissoc db [:prev pred] [:delta pred]))
+              db rec-preds)))))))
 
-(defn eval-pgm [db strata]
-  (reduce eval-naive-step db strata))
+(defn eval-seminaive-strata [db seminaive-strata]
+  (reduce eval-seminaive-stratum db seminaive-strata))
 
-(defn scc-kind [scc rules-by-name sccs-graph]
-  (let [n (reduce
-           (fn [m n]
-             (case n
-               3 (reduced 3)
-               (max m n)))
-           0
-           (for [rule-name scc
-                 [_ & body] (rules-by-name rule-name)]
-             (reduce
-               (fn [n [op x]]
-                 (if (= 'not op)
-                   (if (scc (first x))
-                     (reduced 3)
-                     n)
-                   (if (scc op)
-                     (min 2 (inc n))
-                     n)))
-               0
-               body)))]
-    (case n
-      0 :acyclic-singleton
-      1 :linear
-      2 :non-linear
-      3 :non-stratified-negation)))
+(defn eval-rules [db rules]
+  (eval-seminaive-strata db (map seminaive-stratum (stratify rules))))
 
 (defn unwind [v->u u<-v v<-u u v]
   (loop [v->u (assoc v->u v u) u u]
