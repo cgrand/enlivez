7f9953a4c7628519593c68ed584008649356a0d8
Christophe Grand <christophe@cgrand.net>
2019-10-21 14:57:47 +0200

    extended syntax for defrule/defcase

--- src/enlivez/core.clj
+++ src/enlivez/core.clj
@@ -68,23 +68,24 @@
 
 (s/def ::defrule
   (s/cat :name simple-symbol? :doc (s/? string?)
-    :args vector? :clauses (s/* any?)))
+    :args vector? :kind #{:- :=} :clauses (s/* any?)))
 
 (s/def ::defcase
   (s/cat :name symbol? :hint (s/? keyword?)
-    :args vector? :clauses (s/* any?)))
+    :args vector? :kind #{:- :=} :clauses (s/* any?)))
 
 (defmacro defrule
   "Defines an EZ rule with an optional implementation."
   {:arglists '([name doc? [args*]]
                 [name doc? [args*] clause+])}
   [& body]
-  (when-valid [{:keys [doc args clauses] the-name :name} ::defrule body]
-    (let [pure-args (into [] (map #(if (symbol? %) % (gensym "_"))) args)
+  (when-valid [{:keys [doc args kind clauses] the-name :name} ::defrule body]
+    (let [pure-args (cond-> (into [] (map #(if (symbol? %) % (gensym "_"))) args)
+                      (and (= := kind) (not-any? #{'%} args)) (conj '%))
           decl `(def ~(cond-> (vary-meta the-name assoc :arglists `'~(list pure-args) ::rule true)
                         doc (vary-meta assoc :doc doc)) {})]
       (if clauses
-        `(do ~decl (defcase ~the-name ::defrule ~args ~@clauses))
+        `(do ~decl (defcase ~the-name ::defrule ~args ~kind ~@clauses))
         decl))))
 
 (defn- quote-clause [[pred & args :as clause]]
@@ -179,20 +180,24 @@
 (defmacro defcase
   {:arglists '([name hint? [args*] clause+])}
   [& body]
-  (when-valid [{:keys [hint args clauses] the-name :name} ::defcase body]
+  (when-valid [{:keys [hint args kind clauses] the-name :name} ::defcase body]
     (let [hint (or hint (keyword (gensym "case")))
           full-name (symbol (-> *ns* ns-name name) (name the-name))
           veqs (volatile! [])
           args (into []
-                 (map #(if (coll? %)
+                 (map #(if (symbol? %)
+                         %
                          (let [arg (gensym "arg")]
                            (vswap! veqs conj (list 'eq % arg))
-                           arg)
-                         %))
+                           arg)))
                  args)
-          expansion (into @veqs (or (impl/unnest (cons 'vector args)) [(cons 'vector args)]))
-          clauses (into (pop expansion) clauses)
-          args (into [] (next (peek expansion)))
+          args (cond-> args
+                 (and (= := kind) (not-any? #{'%} args))
+                 (conj '%))
+          clauses (case kind
+                    :- (vec clauses)
+                    := [(list 'eq '% (cons 'and clauses))])
+          clauses (into clauses @veqs)
           {:keys [deps expansion]} (analyze-case &env full-name args clauses)
           expr `{::case '~(cons (cons full-name args) clauses)
                  ::expansion ~expansion
